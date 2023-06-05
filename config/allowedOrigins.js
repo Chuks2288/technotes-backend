37583463b1a6577713d36a822fdc210347297fd9
unknown <dumbirichuks@gmail.com>
2023-06-05 08:28:34 +0100

    mern_stack backend

---
+++ config/allowedOrigins.js
@@ -0,0 +1,12 @@
+const allowedOrigins = [
+    // 'http://localhost:3000',
+    'https://www.technotes.onrender.com',
+    // 'http://dandrepairshop.com'
+]
+
+module.exports = allowedOrigins;
+
+
+
+//cors is installed and imported in serverjs,
+//it allows view our document from this registered ports apart from the normal default port
